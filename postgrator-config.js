206346f28de0c522981afe60302d7cb9dcec1b90
antonibrivera <josephr98@gmail.com>
2020-06-25 15:32:01 -0400

    adjustment to 'postgrator-config.js' and 'package,json'

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,6 +6,7 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
+  "ssl": !! process.env.SSL,
   "host": process.env.MIGRATION_DB_HOST,
   "port": process.env.MIGRATION_DB_PORT,
   "database": process.env.MIGRATION_DB_NAME,
