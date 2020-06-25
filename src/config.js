3cf12887112d31bc4b0833cdc51517fd097d6294
antonibrivera <josephr98@gmail.com>
2020-06-25 13:42:44 -0400

    preparring for deploy

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DB_URL: process.env.DATABASE_URL
+  DATABASE_URL: process.env.DATABASE_URL
     || 'postgresql://localhost/spaced-repetition',
-  TEST_DB_URL: process.env.TEST_DATABASE_URL
+  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL
     || 'postgresql://localhost/spaced-repetition-test', 
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
